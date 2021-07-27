5f72934861a0e2514af13bae4f00a215fdbf114e
Michael Gold <mgold@spscommerce.com>
2021-07-26 22:04:28 -0400

    Matrix inversion

--- src/Matrix.hs
+++ src/Matrix.hs
@@ -4,9 +4,23 @@ import Data.Array (Array, array, assocs, bounds, elems, indices, ixmap, listArra
 import qualified Data.List as List
 import Data.Maybe (fromMaybe, mapMaybe)
 import Tuple (T (T), toTuple)
+import Utils
 
 newtype Matrix = Matrix {asArray :: Array (Int, Int) Double}
-    deriving (Show, Eq)
+    deriving (Show)
+
+instance Eq Matrix where
+    (Matrix m1) == (Matrix m2) = all (\(((r1, c1), v1), ((r2, c2), v2)) -> r1 == r2 && c1 == c2 && v1 `eq` v2) $ zip (assocs m1) (assocs m2)
+
+rows :: Matrix -> Int
+rows (Matrix m) =
+    let (_, (rows, cols)) = bounds m
+     in rows + 1
+
+cols :: Matrix -> Int
+cols (Matrix m) =
+    let (_, (rows, cols)) = bounds m
+     in cols + 1
 
 mkMatrix :: Int -> Int -> [((Int, Int), Double)] -> Matrix
 mkMatrix rows cols d =
@@ -27,19 +41,19 @@ get coord (Matrix m)
     | coord `elem` indices m = Just $ m ! coord
     | otherwise = Nothing
 
-getAllInRow :: Int -> Array (Int, Int) Double -> [(Int, Int)]
+getAllInRow :: Int -> Array (Int, Int) Double -> [((Int, Int), Double)]
 getAllInRow row m =
-    mapMaybe (\(coord@(r, _), _) -> if r == row then Just coord else Nothing) $ assocs m
+    mapMaybe (\a@((r, _), _) -> if r == row then Just a else Nothing) $ assocs m
 
-getAllInCol :: Int -> Array (Int, Int) Double -> [(Int, Int)]
-getAllInCol col m = mapMaybe (\(coord@(_, c), _) -> if c == col then Just coord else Nothing) $ assocs m
+getAllInCol :: Int -> Array (Int, Int) Double -> [((Int, Int), Double)]
+getAllInCol col m = mapMaybe (\a@((_, c), _) -> if c == col then Just a else Nothing) $ assocs m
 
 multM :: Matrix -> Matrix -> Matrix
 multM (Matrix m1) (Matrix m2) =
     let getCell :: (Int, Int) -> Double
         getCell (row, col) =
-            let allForRow = getAllInRow row m1
-                allForCol = getAllInCol col m2
+            let allForRow = fst <$> getAllInRow row m1
+                allForCol = fst <$> getAllInCol col m2
              in sum $ (\(m1Coord, m2Coord) -> (m1 ! m1Coord) * (m2 ! m2Coord)) <$> zip allForRow allForCol
         rowsInM = fst . snd . bounds
         colsInM = snd . snd . bounds
@@ -56,12 +70,16 @@ transposeM (Matrix m) =
      in Matrix $ ixmap ((0, 0), (cols, rows)) (\(row, col) -> (col, row)) m
 
 determinant :: Matrix -> Double
-determinant m =
-    let a = (0, 0) `get` m
-        b = (0, 1) `get` m
-        c = (1, 0) `get` m
-        d = (1, 1) `get` m
-     in fromMaybe 0 $ (\a' b' c' d' -> a' * d' - b' * c') <$> a <*> b <*> c <*> d
+determinant m@(Matrix a)
+    | rows m > 2 && cols m > 2 =
+        let row = (\((r, c), v) -> v * cofactor r c m) <$> getAllInRow 0 a
+         in sum row
+    | otherwise =
+        let a = (0, 0) `get` m
+            b = (0, 1) `get` m
+            c = (1, 0) `get` m
+            d = (1, 1) `get` m
+         in fromMaybe 0 $ (\a' b' c' d' -> a' * d' - b' * c') <$> a <*> b <*> c <*> d
 
 submatrix :: Int -> Int -> Matrix -> Matrix
 submatrix row col (Matrix m) =
@@ -72,3 +90,21 @@ submatrix row col (Matrix m) =
 
 minor :: Int -> Int -> Matrix -> Double
 minor row col = determinant . submatrix row col
+
+cofactor :: Int -> Int -> Matrix -> Double
+cofactor row col m
+    | even (row + col) = minor row col m
+    | otherwise = - minor row col m
+
+data Invertibility = Invertible | NotInvertible
+    deriving (Show, Eq)
+
+invertible :: Matrix -> Invertibility
+invertible m
+    | determinant m == 0 = NotInvertible
+    | otherwise = Invertible
+
+inverse :: Matrix -> Maybe Matrix
+inverse m@(Matrix _) | invertible m == NotInvertible = Nothing
+inverse m@(Matrix a) =
+    Just $ mkMatrix (rows m) (cols m) $ (\((row, col), v) -> ((col, row), cofactor row col m / determinant m)) <$> assocs a
